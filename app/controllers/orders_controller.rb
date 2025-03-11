ca759e56faa90a078f09b6b3dd462dbb919ef5e8
Parth Tagline <prs@taglineinfotech.com>
2025-03-11 15:47:56 +0530

    Optimized orders controller changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,33 @@
 class OrdersController < ApplicationController
-  def create
-    user = User.find_by(email: params[:email].downcase)
-    
-    unless user
-      user = User.new(name: params[:name], email: params[:email])
-      if user.save
-        NotificationMailer.welcome_email(user).deliver_now
-      end
-    end
+  before_action :find_or_create_user, only: :create
 
-    order = Order.new(
-      user_id: user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      total_price: Product.find(params[:product_id]).price * params[:quantity].to_i
-    )
+  def create
+    order = @user.orders.new(order_params)
 
     if order.save
-      NotificationMailer.order_confirmation(order).deliver_now
-      render json: { message: "Order placed successfully" }, status: :created
+      render_success("Order placed successfully", OrderSerializer.new(order).serializable_hash, :created)
     else
-      render json: { error: order.errors.full_messages }, status: :unprocessable_entity
+      render_error("Order creation failed", order.errors.full_messages, :unprocessable_entity)
     end
+  end
+
+  private
+
+  def find_or_create_user
+    @user = User.find_or_create_by(email: user_params[:email]) do |usr|
+      usr.name = user_params[:name]
+    end
+
+    render_error("User creation failed", @user.errors, :unprocessable_entity) if @user.errors.any?
+  end
+
+  def order_params
+    product = Product.find(params[:product_id])
+    total_price = product.price * params[:quantity].to_i
+    params.permit(:product_id, :quantity).merge(total_price: total_price)
+  end
+
+  def user_params
+    params.permit(:email, :name)
   end
 end
