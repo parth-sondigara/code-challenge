e333723afbe44a957c335c945f41a2dd1cc6ed56
Parth Tagline <prs@taglineinfotech.com>
2025-03-11 15:36:52 +0530

    Added changes for the error handling and routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  defaults format: :json do
+    resources :orders, only: [ :create ]
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
